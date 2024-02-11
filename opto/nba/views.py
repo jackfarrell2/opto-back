72f794fffd6f70916e5a8fdf4a53e39801b5f46f
Jack <jackfarrell860@gmail.com>
2024-02-11 03:47:37 -0500

    completed updating slate functionality, completed clean up lineup card, began lineupdash ui

--- opto/nba/views.py
+++ opto/nba/views.py
@@ -18,26 +18,24 @@ from fuzzywuzzy import fuzz
 from .utils import player_mappings
 import openpyxl
 from datetime import datetime, timedelta, timezone
+import pytz
 
 
 @api_view(['GET'])
 def get_slates(request):
     try:
         current_datetime_utc = datetime.now(timezone.utc)
-        next_930_utc = current_datetime_utc.replace(
+        slate_cutoff = current_datetime_utc.replace(
             hour=9, minute=30, second=0, microsecond=0)
-        if current_datetime_utc > next_930_utc:
-            # If it's already past 4:30 AM, move to the next day
-            next_930_utc += timedelta(days=1)
-
+        slate_cutoff -= timedelta(days=1)
         future_slates = Slate.objects.filter(
-            sport='NBA', date__gte=next_930_utc).order_by('date')
+            sport='NBA', date__gte=slate_cutoff).order_by('date')
 
         if future_slates.exists():
             slates = future_slates
         else:
             nearest_upcoming_slate = Slate.objects.filter(
-                sport='NBA', date__lt=next_930_utc).order_by('-date').first()
+                sport='NBA').order_by('-date').first()
             slates = [nearest_upcoming_slate] if nearest_upcoming_slate else []
 
         formatted_slates = []
@@ -207,114 +205,125 @@ def upload_projections(request):
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 def add_slate(request):
-    if request.user.is_staff == False:
-        return Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
-    # Get uploaded csv file
-    if request.data['projections-only'] == 'true':
-        default_projections = request.FILES['file-two']
-        slate = Slate.objects.get(pk=int(request.data['slate']))
-        update_default_projections(slate.id, default_projections)
-        for player in Player.objects.filter(slate=slate):
-            new_num = randomize_within_percentage(
-                float(player.projection), 7.5)
-            player.projection = new_num
+    try:
+        if request.user.is_staff == False:
+            return Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        # Get uploaded csv file
+        if request.data['projections-only'] == 'true':
+            default_projections = request.FILES['file-two']
+            slate = Slate.objects.get(pk=int(request.data['slate']))
+            update_default_projections(slate.id, default_projections)
+            for player in Player.objects.filter(slate=slate):
+                new_num = randomize_within_percentage(
+                    float(player.projection), 7.5)
+                player.projection = new_num
+                player.save()
+            return Response({})
+        try:
+            slate_file = request.FILES['file-one']
+        except:
+            return Response({"error": "No lineup file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            default_projections = request.FILES['file-two']
+        except:
+            default_projections = None
+        csv = DictReader(iterdecode(slate_file, 'utf-8'))
+        # Gather slates game info
+        game_times = []
+        teams = []
+        games = []
+        for row in csv:
+            # Populate game times, teams, and games
+            game_info = row['Game Info']
+            if game_info == '-':
+                # Pass players with no games
+                continue
+            if game_info not in games:
+                games.append(game_info)  # Add game
+            game_teams, game_time = game_info.split(" ", 1)
+            away_team, home_team = game_teams.split('@')
+            # Add teams
+            if ({'team': away_team, 'opponent': home_team}) not in teams:
+                teams.append({'team': away_team, 'opponent': home_team})
+            if ({'team': home_team, 'opponent': away_team}) not in teams:
+                teams.append({'team': home_team, 'opponent': away_team})
+            # Add game times
+            if game_time not in game_times:
+                game_times.append(game_time)
+        game_count = int(len(teams) / 2)  # Game Count
+        # Store slate time
+        earliest_game = sorted(game_times)[0]
+        earliest_game = earliest_game[:-3]  # Strip time zone
+        earliest_game = datetime.strptime(earliest_game, '%m/%d/%Y %I:%M%p')
+        EDT = ZoneInfo('US/Eastern')  # Avoid naive datetime
+        earliest_game = earliest_game.replace(tzinfo=EDT)
+        # Create slate
+        slate = Slate(date=earliest_game, game_count=game_count, sport='NBA')
+        slate.save()
+        # Create teams for the slate
+        for team in teams:
+            # Add teams
+            this_team = Team.objects.create(
+                abbrev=team['team'], opponent=team['opponent'], slate=slate)
+            this_team.save()
+        for game in games:
+            # Add games
+            teams, time = game.split(' ', 1)
+            # Add home and away teams
+            away_team, home_team = teams.split('@')
+            home_team = Team.objects.get(abbrev=home_team, slate=slate)
+            away_team = Team.objects.get(abbrev=away_team, slate=slate)
+            # Add game time
+            time = time[:-3]
+            time = datetime.strptime(time, '%m/%d/%Y %I:%M%p')
+            time = earliest_game.replace(tzinfo=EDT)  # Avoid naive datetime
+            this_game = Game.objects.create(time=time, home_team=home_team,
+                                            away_team=away_team, slate=slate)
+            this_game.save()
+        # Save players and positions
+        csv = DictReader(iterdecode(slate_file, 'utf-8'))
+        for row in csv:
+            players_positions = row['Roster Position'].split('/')
+            default_position = row['Position']
+            position_flags = {'F': False, 'C': False, 'G': False, 'SG': False,
+                            'PG': False, 'SF': False, 'PF': False, 'UTIL': False}
+            for position in players_positions:
+                if position in position_flags:
+                    position_flags[position] = True
+            team = Team.objects.get(abbrev=row['TeamAbbrev'], slate=slate)
+            opponent = team.opponent
+            projection = 5
+
+            # Add player
+            player = Player(name=row['Name'],
+                            projection=projection,
+                            team=team,
+                            opponent=opponent,
+                            dk_id=row['ID'],
+                            salary=row['Salary'],
+                            slate=slate,
+                            F=position_flags['F'],
+                            C=position_flags['C'],
+                            G=position_flags['G'],
+                            SG=position_flags['SG'],
+                            PG=position_flags['PG'],
+                            SF=position_flags['SF'],
+                            PF=position_flags['PF'],
+                            UTIL=position_flags['UTIL'],
+                            position=default_position)
             player.save()
+        # Add default projections
+        if default_projections:
+            update_default_projections(slate.id, default_projections)
+            for player in Player.objects.filter(slate=slate):
+                new_num = randomize_within_percentage(float(player.projection), 7.5)
+                player.projection = new_num
+                player.save()
+
         return Response({})
-    slate_file = request.FILES['file-one']
-    default_projections = request.FILES['file-two']
-    csv = DictReader(iterdecode(slate_file, 'utf-8'))
-    # Gather slates game info
-    game_times = []
-    teams = []
-    games = []
-    for row in csv:
-        # Populate game times, teams, and games
-        game_info = row['Game Info']
-        if game_info == '-':
-            # Pass players with no games
-            continue
-        if game_info not in games:
-            games.append(game_info)  # Add game
-        game_teams, game_time = game_info.split(" ", 1)
-        away_team, home_team = game_teams.split('@')
-        # Add teams
-        if ({'team': away_team, 'opponent': home_team}) not in teams:
-            teams.append({'team': away_team, 'opponent': home_team})
-        if ({'team': home_team, 'opponent': away_team}) not in teams:
-            teams.append({'team': home_team, 'opponent': away_team})
-        # Add game times
-        if game_time not in game_times:
-            game_times.append(game_time)
-    game_count = int(len(teams) / 2)  # Game Count
-    # Store slate time
-    earliest_game = sorted(game_times)[0]
-    earliest_game = earliest_game[:-3]  # Strip time zone
-    earliest_game = datetime.strptime(earliest_game, '%m/%d/%Y %I:%M%p')
-    EDT = ZoneInfo('US/Eastern')  # Avoid naive datetime
-    earliest_game = earliest_game.replace(tzinfo=EDT)
-    # Create slate
-    slate = Slate(date=earliest_game, game_count=game_count, sport='NBA')
-    slate.save()
-    # Create teams for the slate
-    for team in teams:
-        # Add teams
-        this_team = Team.objects.create(
-            abbrev=team['team'], opponent=team['opponent'], slate=slate)
-        this_team.save()
-    for game in games:
-        # Add games
-        teams, time = game.split(' ', 1)
-        # Add home and away teams
-        away_team, home_team = teams.split('@')
-        home_team = Team.objects.get(abbrev=home_team, slate=slate)
-        away_team = Team.objects.get(abbrev=away_team, slate=slate)
-        # Add game time
-        time = time[:-3]
-        time = datetime.strptime(time, '%m/%d/%Y %I:%M%p')
-        time = earliest_game.replace(tzinfo=EDT)  # Avoid naive datetime
-        this_game = Game.objects.create(time=time, home_team=home_team,
-                                        away_team=away_team, slate=slate)
-        this_game.save()
-    # Save players and positions
-    csv = DictReader(iterdecode(slate_file, 'utf-8'))
-    for row in csv:
-        players_positions = row['Roster Position'].split('/')
-        default_position = row['Position']
-        position_flags = {'F': False, 'C': False, 'G': False, 'SG': False,
-                          'PG': False, 'SF': False, 'PF': False, 'UTIL': False}
-        for position in players_positions:
-            if position in position_flags:
-                position_flags[position] = True
-        team = Team.objects.get(abbrev=row['TeamAbbrev'], slate=slate)
-        opponent = team.opponent
-        projection = 0
-
-        # Add player
-        player = Player(name=row['Name'],
-                        projection=projection,
-                        team=team,
-                        opponent=opponent,
-                        dk_id=row['ID'],
-                        salary=row['Salary'],
-                        slate=slate,
-                        F=position_flags['F'],
-                        C=position_flags['C'],
-                        G=position_flags['G'],
-                        SG=position_flags['SG'],
-                        PG=position_flags['PG'],
-                        SF=position_flags['SF'],
-                        PF=position_flags['PF'],
-                        UTIL=position_flags['UTIL'],
-                        position=default_position)
-        player.save()
-    # Add default projections
-    update_default_projections(slate.id, default_projections)
-    for player in Player.objects.filter(slate=slate):
-        new_num = randomize_within_percentage(float(player.projection), 7.5)
-        player.projection = new_num
-        player.save()
-
-    return Response({})
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['PUT'])
