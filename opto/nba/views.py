eb9a42b2e05c96c27bbc22a682c73464211ce9c9
Jack Farrell <jackfarrell860@gmail.com>
2024-11-16 17:01:25 -0500

    began work on multi-slate proj upload feature

--- opto/nba/views.py
+++ opto/nba/views.py
@@ -71,6 +71,7 @@ def upload_projections(request):
         elif method == 'paste':
             paste_projections = request.data['paste-projections']
             projections = json.loads(paste_projections)
+        # need to potentially update projections for all slates within the same day (not necessarily the same 24 hours, also need to consider double headers)
         slate = Slate.objects.get(id=int(request.data['slate']))
         user = request.user
         all_players = Player.objects.filter(slate=slate)
