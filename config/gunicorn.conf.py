9b4eff5f0160454438db7db8793a858840431ee1
Jack Farrell <jackfarrell860@gmail.com>
2026-03-02 17:11:14 -0500

    added nginx configuration, migrated to cheaper server

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,4 +1,4 @@
-bind = "0.0.0.0:80"
+bind = "127.0.0.1:8000"
 workers = 3
 accesslog = "/app/logs/gunicorn.access.log"
 errorlog = "/app/logs/gunicorn.app.log"
--- opto/opto/settings/prod.py
+++ opto/opto/settings/prod.py
@@ -9,7 +9,7 @@ import db_info  # noqa
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = False
-ALLOWED_HOSTS = ['apidfsopto.xyz', 'www.apidfsopto.xyz']
+ALLOWED_HOSTS = ['apidfsopto.xyz', 'www.apidfsopto.xyz', '104.131.2.205']
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CORS_ALLOWED_ORIGINS = [
