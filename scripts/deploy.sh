8fa22698bdf587196849cda79d896ca8cb19047a
Jack Farrell <jackfarrell860@gmail.com>
2026-09-08 21:31:26 -0400

    turned off automatic db backup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 # Deploy Django project
 export SERVER=optoback
-./scripts/backup-database.sh
+# ./scripts/backup-database.sh
+# backups take too much space on cheap server, only use manually
 ./scripts/upload-code.sh
 ./scripts/install-code.sh
